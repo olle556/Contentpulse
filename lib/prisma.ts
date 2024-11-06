aa72769e3f649262ce691ba2249c8b2de31925dd
olle556 <olleevertsson@gmail.com>
2024-11-06 11:59:36 +0100

    fixade connection till supabase med nya credentials

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -8,7 +8,7 @@ export const prisma = globalForPrisma.prisma ?? new PrismaClient({
   log: ['query', 'error', 'warn'],
   datasources: {
     db: {
-      url: process.env.POSTGRES_URL
+      url: process.env.DATABASE_URL
     }
   }
 })
