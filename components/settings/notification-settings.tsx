4a4b6e1096ba196a90b1c8d0afb82a983254ec0a
olle556 <olleevertsson@gmail.com>
2024-11-10 16:25:28 +0100

    tog bort "performance saken i settings"

--- components/settings/notification-settings.tsx
+++ components/settings/notification-settings.tsx
@@ -70,7 +70,7 @@ export function NotificationSettings() {
               <div className="space-y-0.5">
                 <FormLabel className="text-base">Schedule Reminders</FormLabel>
                 <FormDescription>
-                  Get reminders about upcoming scheduled posts.
+                  Get reminded when new posts have been generated.
                 </FormDescription>
               </div>
               <FormControl>
@@ -82,28 +82,6 @@ export function NotificationSettings() {
             </FormItem>
           )}
         />
-
-        <FormField
-          control={form.control}
-          name="performanceUpdates"
-          render={({ field }) => (
-            <FormItem className="flex flex-row items-center justify-between rounded-lg border p-4">
-              <div className="space-y-0.5">
-                <FormLabel className="text-base">Performance Updates</FormLabel>
-                <FormDescription>
-                  Receive updates about your content performance.
-                </FormDescription>
-              </div>
-              <FormControl>
-                <Switch
-                  checked={field.value}
-                  onCheckedChange={field.onChange}
-                />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-
         <Button type="submit">Save Preferences</Button>
       </form>
     </Form>
