98f5fee201fb0c44806920f45b5b3c1b0a420d91
jespermollbrant <jesperviktormollbrant@gmail.com>
2024-11-29 17:32:06 +0100

    hej

--- app/api/check-subscription_2/route.ts
+++ app/api/check-subscription_2/route.ts
@@ -8,8 +8,7 @@ import { checkSubscription } from '@/lib/subscription';
 // Check if the user is authorized to access the protected generation paths BY USING the checksuSubscription 
 // skapa en till route för säkerhetsskull. Vill inte ändra i din orginal route. 
 
-export const dynamic = 'force-dynamic'; // Add this line
-export const runtime = 'edge'; // Optional: Add this if you want to use edge runtime
+export const dynamic = 'force-dynamic';
 
 export async function GET() {
   try {
