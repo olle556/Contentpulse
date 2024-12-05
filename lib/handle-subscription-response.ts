8a63486f4ba7c4efe7673fdc4cc759687d91da5c
jespermollbrant <jesperviktormollbrant@gmail.com>
2024-12-05 11:32:17 +0100

    removed unused

--- lib/handle-subscription-response.ts
+++
@@ -1,9 +0,0 @@
-import { toast } from "sonner";
-
-export async function handleSubscriptionResponse(response: Response) {
-  if (response.status === 403) {
-    await response.json();
-    return false;
-  }
-  return true;
-}
